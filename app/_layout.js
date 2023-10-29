522ee309b02e0009e0e2aaf04df83ac8d172ab2a
Emmanuel Sackey <aximilli1212@gmail.com>
2023-10-29 10:47:51 +0000

    install expo fonts

--- app/_layout.js
+++ app/_layout.js
@@ -1,10 +1,23 @@
-import {Stack} from 'expo-router'
+import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
 
-const Layout = ({children}) => {
+
+const Layout = () => {
+    const [fontsLoaded] = useFonts({
+        DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+        DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+        DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+    });
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
     return (
         <Stack>
-        {children}
+            <Stack.Screen name="home" />
         </Stack>
     )
-}
+};
+
 export default Layout;
