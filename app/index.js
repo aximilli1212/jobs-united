4ead61fc5c47b033b686338207a98965ba68b03b
Emmanuel Sackey <aximilli1212@gmail.com>
2023-10-29 09:16:07 +0000

    Add header leading and trailing actions

--- app/index.js
+++ app/index.js
@@ -1,10 +1,31 @@
-import {View, Text} from 'react-native';
+import {View, Text, SafeAreaView} from 'react-native';
+import { Stack, useRouter } from "expo-router";
+import {
+    Nearbyjobs,
+    Popularjobs,
+    ScreenHeaderBtn,
+    Welcome,
+} from "../components";
+import {COLORS, icons, images, SIZES } from "../constants";
 
 const Home = () => {
     return (
-        <View>
+        <SafeAreaView style={{flex: 1, backgroundColor: COLORS.lightWhite}}>
         <Text>Home</Text>
-        </View>
+            <Stack.Screen>
+                options={{
+                    headerStyle: {backgroundColor: COLORS.lightWhite},
+                    headerShadowVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn iconUrl={icons.menu} dimension='60%' />
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn iconUrl={images.profile} dimension='100%' />
+                    ),
+                    headerTitle: "",
+            }}
+            </Stack.Screen>
+        </SafeAreaView>
     );
 }
 export default Home;
