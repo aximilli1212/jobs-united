dce5c5aba65b3f61fe35edf79322d933dafc57a9
Emmanuel Sackey <aximilli1212@gmail.com>
2023-10-29 11:57:32 +0000

    update env lib

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const useFetch = (endpoint, query) => {
+    const rapidApiKey = "479f71b094msh590611df11313ebp1e80bbjsn612b832d928e"
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
